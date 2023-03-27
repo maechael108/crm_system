6b30d47f7b79240d7c1e8a8513877849541a485b
maechael108 <maechael@pbibinc@gmail.com>
2023-03-27 15:52:35 -0700

    commit inital table

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -2,10 +2,10 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
 import Menubar from 'primevue/menubar';
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
+import ColumnGroup from 'primevue/columngroup';   // optional
 </script>
-
-
-    
 
 <template>
     
@@ -18,8 +18,40 @@ import Menubar from 'primevue/menubar';
        
         <div class="py-12">
             <div>
-              
+                <DataTable :value="products" tableStyle="min-width: 50rem">
+                  <Column field="code" header="Code"></Column>
+                  <Column field="name" header="Name"></Column>
+                  <Column field="category" header="Category"></Column>
+                  <Column field="quantity" header="Quantity"></Column>
+                </DataTable>
+    
             </div>
         </div>
     </AuthenticatedLayout>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      products: [
+        { code: "001", name: "Product A", category: "Category A", quantity: 10 },
+        { code: "002", name: "Product B", category: "Category B", quantity: 20 },
+        { code: "003", name: "Product C", category: "Category C", quantity: 30 },
+      ],
+    };
+  },
+
+  methods: {
+    addRow() {
+      const newRow = {
+        code: "004",
+        name: "Product D",
+        category: "Category D",
+        quantity: 40,
+      };
+
+      this.products.push(newRow);
+    },
+  },
+};
+</script>
